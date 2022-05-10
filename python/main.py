0ae89f1f6bfca8fb2dd6c8973bd527b5352b9ddf
yuwenhao <yuwenhao@stu.pku.edu.cn>
2022-05-10 08:30:02 +0800

    上位机框架更新

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
 import serial;
+import tkinter;
 
-try:
+serialPort = "COM4"  # 串口
+baudRate = 115200 # 波特率
+ser = serial.Serial(serialPort, baudRate, timeout=0.5)
+print("参数设置：串口=%s ，波特率=%d" % (serialPort, baudRate))
 
-
-except Exception as e:
